452ec189ce207299a93daa6c040cb0f992019b05
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-01-23 11:53:35 +0300

    initial commit

---
+++ horoscope/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def leo(request):
+    return HttpResponse('Знак зодиака лев')
+
+
+def scorpio(request):
+    return HttpResponse('Знак зодиака скорпион')
+
+
+def intro(request):
+    return HttpResponse('111111')
